a0d40103278402ff0e16f39a41adc590a072e4b7
Heron-pc <philipyizhou.huang@mail.utoronto.ca>
2022-06-09 16:05:37 -0400

    update ping1d driver

--- scripts/driver.py
+++ scripts/driver.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Range
 from brping import Ping1D
@@ -6,17 +7,23 @@ from brping import Ping1D
 def publish_sonar_data():
     # Specification of the sonar sensor is available here:
     # https://www.bluerobotics.com/store/sensors-sonars-cameras/sonar/ping-sonar-r2-rp/
+    rospy.init_node("single_beam_sonar_node")
+
     usb_port = rospy.get_param('usb_port', '/dev/ttyUSB0')
     frequency = rospy.get_param('frequency', 10)
-    frame = rospy.get_param('frame', "odom")
-    fov = rospy.get_param('fov', 0.3)
+    frame = rospy.get_param('frame', "ping1d_link")
+    fov = rospy.get_param('fov', 0.523)
     min_range = rospy.get_param("min_range", 0.5)
     max_range = rospy.get_param("max_range", 50.0)
 
     sonar_driver = Ping1D()
     sonar_driver.connect_serial(device_name=usb_port)
-    rospy.init_node("single_beam_sonar_node")
-    pub_sonar = rospy.Publisher("sonar", Range, queue_size=1)
+    if sonar_driver.initialize() is False:
+        print("Fail to initialize Ping!")
+        exit(1)
+
+    rospy.loginfo("connecting to Ping1D Sonar at {}".format(usb_port))
+    pub_sonar = rospy.Publisher("/ping1d/range", Range, queue_size=1)
     range_msg = Range()
     range_msg.header.frame_id = frame
     range_msg.radiation_type = 1
@@ -31,8 +38,8 @@ def publish_sonar_data():
         range_msg.range = data["distance"] / 1000.0
         range_msg.header.stamp = rospy.Time.now()
         pub_sonar.publish(range_msg)
-        rospy.loginfo(
-            f"Distance: {range_msg.range: .4f}\tConfidence: {confidence: .0f}")
+        # rospy.loginfo(
+        #     f"Distance: {range_msg.range: .4f}\tConfidence: {confidence: .0f}")
         rate.sleep()
 
 
